16d573fbf8297c4c14306768b622c3087d9732a2
Chekrishsuper <78082844+Chekrishsupra@users.noreply.github.com>
2021-04-14 17:14:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+menu_list_array = ["Chicken Tandoori Pizza","Panner Tikka Pizza","Veg Supreme Pizza","Duluxe Veggie Pizza","Veg Extravaganza Pizza"];
+
+function getmenu() {
+    var htmldata;
+    htmldata= "<ol class='menulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++) {
+        html=htmldata+'<li>'+ menu_list_array[i] + '</li>'
+    }
+    htmldata=htmldata+"</ol>"
+    document.getElementById("display").innerHTML = htmldata;
+}
+function add_item() {
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    for(var i=0;i<menu_list_array.length;i++){
+
+    htmldata+htmldata+'<div class="card">' +'img src=pizzaImg.png"/>"' + '</div>'
+    }
+
+    htmldata=htmldata+"</section>"
+    document.getElementById("added_menu").innerHTML = htmldata;
+}
